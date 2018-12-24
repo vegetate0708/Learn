78e9ba112d4558b5ac5cfcdc3a33e0967269f38a
vegetate0708 <252295126@qq.com>
2018-12-24 17:05:32 +0800

    队列的实现

--- DataStructuresAndAlgorithms/src/com/vegetate/Algorithms/MyQueue.java
+++ DataStructuresAndAlgorithms/src/com/vegetate/Algorithms/MyQueue.java
@@ -17,9 +17,56 @@ package com.vegetate.Algorithms;
  * @since 1.0.0
  */
 public class MyQueue {
+    //大小
     private int maxSize;
+    //队头位置
     private int font;
+    //队尾
     private int end;
     private long[] arr;
+    //有效数
     private int elems;
+
+    public MyQueue(int maxSize) {
+        this.maxSize = maxSize;
+        arr=new long[maxSize];
+        font=0;
+        end=-1;
+        elems=0;
+    }
+
+    public void insert(long value){
+        if(!isFull()){
+            arr[++end]=value;
+            elems++;
+        }
+
+    }
+
+    public long remove(){
+        elems--;
+        return arr[font++];
+    }
+    public boolean isEmpty(){
+        return elems==0;
+    }
+    public boolean isFull(){
+        return end==maxSize-1;
+    }
+
+    public int size(){
+        return elems;
+    }
+
+    public static void main(String[] args){
+          MyQueue myQueue=new MyQueue(3);
+          myQueue.insert(8);
+          myQueue.insert(3);
+          myQueue.insert(6);
+          myQueue.insert(1);
+          while (!myQueue.isEmpty()){
+              System.out.println(myQueue.remove());
+          }
+
+    }
 }
